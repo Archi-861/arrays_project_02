d945a73c40ef090bce97917bf4eb6db927b7d667
Artem <artemkashcheev@yandex.ru>
2024-12-17 22:24:21 +0300

    feat(arrays_lib): Added function prod

--- arrays_lib.py
+++ arrays_lib.py
@@ -13,5 +13,14 @@ def sum_1d(array):
     for i in array:
         summ += i
     return summ
+
+
+def prod_1d(array):
+    prod = 1
+    for i in array:
+        prod *= i
+    return prod
+
 print(array)
 print(sum_1d(array))
+print(prod_1d(array))
