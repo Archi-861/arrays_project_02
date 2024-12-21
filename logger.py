f450a7736446d0d3837f9d98b0838af0b2c70789
Artem <artemkashcheev@yandex.ru>
2024-12-21 21:49:20 +0300

    fix(arrays_lib): Optimaized import os

--- logger.py
+++ logger.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-import os
+from os import stat
 
 def log_action(name):
     time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     message = f'Perfoming {name} on array1 and array2 {time}'
-    if os.stat('log_action.txt').st_size == 0:
+    if stat('log_action.txt').st_size == 0:
         with open('log_action.txt', 'w') as file:
             file.write(message + '\n')
     else:
