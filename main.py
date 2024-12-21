06b7205bf55ca5da2202d831ceff44acb03b10f1
Artem <artemkashcheev@yandex.ru>
2024-12-21 22:12:03 +0300

    feat(arrays_lib): Added function prod_arrays for two arrays

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from arrays_lib import array1, array2, sum_arrays
+from arrays_lib import array1, array2, sum_arrays, prod_1d, prod_arrays
 import logger
 
 print(array1)
 print(array2)
-print(sum_arrays(array1, array2))
+# print(sum_arrays(array1, array2))
+print(prod_arrays(array1, array2))
